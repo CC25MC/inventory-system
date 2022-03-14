b7f03c1f559eb0b8eb9ff4a1cda8f5e86c4d4e97
dercdevil <darwinerc92@gmail.com>
2022-03-14 19:34:23 -0300

    corrigiendo errores de cesar :v

--- src/screens/ClientScreen/View.js
+++ src/screens/ClientScreen/View.js
@@ -2,8 +2,23 @@ import React, { useState } from 'react'
 import { Box, Divider } from '@mui/material'
 import { AppBar, Search, EnhancedTable } from '../../components'
 import { useClients } from '../../Hooks'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 const ClientView = ({}) => {
+
+  const rows = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  ];
+  
+  const columns = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+
+  const datos = { rows, columns }
+
   const { data } = useClients()
   console.log(data)
   return (
@@ -19,7 +34,9 @@ const ClientView = ({}) => {
       <AppBar />
       <Search />
       <Divider />
-      <EnhancedTable />
+      <div style={{ height: 300, width: '100%' }}>
+        <DataGrid {...datos} components={{ Toolbar: GridToolbar }} />
+      </div>
     </Box>
   )
 }
